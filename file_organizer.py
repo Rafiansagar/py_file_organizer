3e5b89fece92bdf74fb7f11ee3483ac0c2f19887
Rafian Sagar <35956615+Rafiansagar@users.noreply.github.com>
2023-02-24 18:08:24 +0600

    Add files via upload

---
+++ file_organizer.py
@@ -0,0 +1,44 @@
+import os
+import shutil
+
+# Path of the folder to be organized
+path = "ecenter-img"
+# path = "Unorganized_Files"
+
+# Dictionary with file types and their respective folder names
+folders = {
+    "doc":  "Documents",
+    "txt":  "Text Files",
+    "pdf":  "PDFs",
+
+    "mp3":  "Music",
+
+    "mp4":  "Videos",
+    "mkv":  "Videos",
+
+    "jpg":  "Images",
+    "jpeg": "Images",
+    "png":  "Images/PNG",
+    "svg":  "Images/SVG",
+    "gif":  "Images/GIF",
+    "webp":  "Images/WEBP",
+
+    "zip":  "Archive",
+    "gz":  "Archive/GZ",
+    "rar":  "Archive/RAR",
+}
+
+# Loop through each file in the folder
+for filename in os.listdir(path):
+    # Get the extension of the file
+    ext = os.path.splitext(filename)[1][1:]
+    
+    # If the file type is in our dictionary, move the file to its respective folder
+    if ext in folders:
+        folder_name = folders[ext]
+        folder_path = os.path.join(path, "Organiged", folder_name)
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+        old_path = os.path.join(path, filename)
+        new_path = os.path.join(folder_path, filename)
+        shutil.move(old_path, new_path)
